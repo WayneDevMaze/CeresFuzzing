dc51ff92a0994e9e90c30acd521a95010994f141
--global <--global>
2020-07-12 09:48:49 +0800

    后台加程序上传接口

--- src/js/fuzz_input.js
+++ src/js/fuzz_input.js
@@ -1,16 +1,76 @@
 
+function visableExample() {
+    console.log("hide examples");
+    $("#examples").hide();
+    // style = "display: none;";
+    // document.getElementById("examples").style.display = "none";
+}
+visableExample();
+
+function show_form(show_case) {
+    $("#fuzztarget").hide();
+    $("#fuzztestcase").hide();
+    $("#fuzzcreater").hide();
+    $("#fuzzdict").hide();
+    $("#fuzzrun").hide();
+    switch (show_case) {
+        case 1:
+            $("#fuzztarget").show();
+            break;
+        case 2:
+            $("#fuzztestcase").show();
+            break;
+        case 3:
+            $("#fuzzcreater").show();
+            break;
+        case 4:
+            $("#fuzzdict").show();
+            break;
+        case 5:
+            $("#fuzzrun").show();
+            break;
+    }
+}
+
+show_form(1);
+
 function fuzztarget() {
-    
+    console.log("set fuzz target");
+    show_form(1);
 }
 function fuzztestcase() {
-    
+    console.log("set fuzz test cases");
+    show_form(2);
 }
 function fuzzcreater() {
-    
+    console.log("set fuzz creater");
+    show_form(3);
 }
 function fuzzdict() {
-    
+    console.log("set fuzz dict");
+    show_form(4);
 }
 function fuzzrun() {
-    alert("run");
+    console.log("run fuzz");
+    show_form(5);
+    //运行fuzz
 }
+
+//layui part
+layui.use('upload', function () {
+    var $ = layui.jquery
+        , upload = layui.upload;
+    //选完文件后不自动上传
+    upload.render({
+        elem: '#choose_target'
+        , url: 'https://httpbin.org/post' //改成您自己的上传接口
+        , auto: false
+        , accept: 'file' //普通文件
+        //,multiple: true
+        , bindAction: '#upload_target'
+        , done: function (res) {
+            layer.msg('上传成功');
+            console.log(res)
+        }
+    });
+})
